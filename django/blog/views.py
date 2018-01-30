566e0cc95be7e7644487e766866fc1f9c624b883
chrisaor <pchpch0070@gmail.com>
2018-01-30 12:03:39 +0900

    templates/blog/post_list.html파일 작성, blog.views.post_list에서 해당 파일 render한 결과 리턴

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-
-# Create your views here.
 
 def post_list(request):
-    return HttpResponse('<html><body><h1>Post list</h1><p>목록을 보여줄 예정입니다</p></body></html>')
+    return render(request, 'blog/post_list.html')
