60df0855fbda6912615cbe8d5a24ea04dabcb058
chrisaor <pchpch0070@gmail.com>
2018-01-30 14:28:44 +0900

    postlist에서 Post QuerySet을 template으로 전달, template tag에서 해당 QuerySeSet을 반복

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+
+
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(
+        request=request,
+        template_name='blog/post_list.html',
+        context=context,
+    )
+    # return render(request, 'blog/post_list.html', context)
 
 
 def post_detail(request):
