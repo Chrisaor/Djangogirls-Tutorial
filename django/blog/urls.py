4f44f4c0707a42a603be52cc51b6e9b55a2c0acb
chrisaor <pchpch0070@gmail.com>
2018-02-01 14:46:51 +0900

    post_add, post_delete구현, post_delete로 연결되는 form을 post_detail.html에 작성

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -1,14 +1,19 @@
-from django.urls import path, re_path
+
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post-list'),
-    # path('', views.post_detail),
-    path('<int:pk>/', views.post_detail, name='post-detail'),
-    # re_path(r'(?P<pk>\d+)/$', views.post_detail),
     # 숫자가 1개이상 반복되는 경우를 정규표현식으로 구현하되
-    # 해당 반복구간을 그룹으로 묶고, 그룹 이름을 pk로 지정
+    # 해당 반복구간을 그룹으로 묶고, 그룹 이름을 'pk'로 지정
     # re.compile(r'(?P<pk>\d+)')
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail),
+    # /3/
+    path('<int:pk>/', views.post_detail, name='post-detail'),
+    # /3/delete/
+    path('<int:pk>/delete/', views.post_delete, name='post-delete'),
+    # /add/
+    path('add/', views.post_add, name='post-add'),
 
-    path('add/', views.post_add, name='post-add')
 ]
